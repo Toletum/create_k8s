d38b19c457ea5565ca7057ac12c9a3843989ed8d
Toletum <toletum@gmail.com>
2025-12-28 11:43:55 +0100

    Bug

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ echo "Waiting for linux to start..."
 for line in $NODES; do
     NODE=$(echo $line | cut -d';' -f1)
     ip=$(echo $line | cut -d';' -f2)
-    until ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -i data/keys root@${ip} "update" > /dev/null 2>&1; do
+    until ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -i data/keys root@${ip} "uptime" > /dev/null 2>&1; do
         sleep 2
     done
 done
