0946630752802d611ae4edd44dd79b20e1a484fb
Toletum <toletum@gmail.com>
2026-01-04 18:30:56 +0100

    http

--- scp.sh
+++ scp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 NODE="$1"
-shift
+FILE="$2"
 
 
 ssh-keygen -f '/home/toletum/.ssh/known_hosts' -R "${NODE}" > /dev/null 2>&1
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -i data/keys -t root@${NODE} $*
+scp -o StrictHostKeyChecking=no -o ConnectTimeout=2 -i data/keys "$FILE" root@${NODE}:
+ 
