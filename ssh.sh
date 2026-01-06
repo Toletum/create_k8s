42b1c3ee76c08d6887a6011ff2755e41314cbd64
Toletum <toletum@gmail.com>
2026-01-06 11:12:38 +0100

    debug

--- ssh.sh
+++ ssh.sh
@@ -3,6 +3,5 @@
 NODE="$1"
 shift
 
-
 ssh-keygen -f '/home/toletum/.ssh/known_hosts' -R "${NODE}" > /dev/null 2>&1
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 -i data/keys -t root@${NODE} $*
